d51aa60682e71a0e30a74c396aa6094caf826718
oleksis <oleksis.fraga@gmail.com>
2020-11-30 19:21:02 -0500

    Add PyInstaller for setup.py and MANIFEST.in

--- README.md
+++ README.md
@@ -1,21 +1,19 @@
-youtube-dl-GUI
-==============
+# youtube-dl-GUI
 
 This repository contains code for a youtube-dl GUI written in PyQt. It is based on [youtube-dl](https://github.com/ytdl-org/youtube-dl) which is a Video downloading script maintained by various contributers and released in Public Domain. This GUI code is currently written for Python v3.x. Please note that this GUI code is released under the MIT License and not Public Domain.
 
-ScreenShot:
--------------
+## ScreenShot:
+
 ![youtube-dl-gui Screenshot in Ubuntu](http://imgur.com/KxTLeYl.png)
 
-Requirements:
-------------
+## Requirements:
+
 - PyQt5
 - [youtube_dl](https://github.com/ytdl-org/youtube-dl)
 - Python >= v3.x
 
+## Features:
 
-Features:
-----------
 This application has the following features:
 - supports downloading videos from 200+ websites
 - allows to download multiple videos in parallel
@@ -23,37 +21,37 @@ This application has the following features:
 - Resumes interrupted downloads
 - Downloads the video in best quality
 
+## Contributor:
 
-Contributor:
----------
 - Muhammad Yasoob Ullah Khalid
 - Oleksis Fraga <oleksis.fraga@gmail.com>
 
 If you want to become a contributor then just contribute some code and I will add you to this list
 
-Bugs:
-----------
+## Bugs:
+
 This program is still in beta so if you encounter any bugs feel free to report them on https://github.com/yasoob/youtube-dl-GUI/issues.
 
-TODO:
--------
+## TODO:
 
-- Change for PyInstaller: setup.py - _see Windows_
 - Fix Convert (main:MainWindow.convert_file)
 - integrate save state
 - Allow pause/resume functionality
 - ~~integrate post processing options~~
 - ~~integrate batch add feature~~
 - ~~integrate Queue to make it stable~~
+- ~~Change for PyInstaller: setup.py - _see Windows_~~
 
-Windows:
------------
-The exe is currently hosted on [Dropbox](https://www.dropbox.com/s/oj8dh4q82tofk34/youtube-dl.exe). I have included a setup.py file. Just run it like this:
-```
-python setup.py py2exe
-```
-This will result in a dist and build directory. Simply go to the dist directory and you will get a ```main.exe``` file and some other files. That ```main.exe``` file is a standalone executable which can be run simply by double clicking it. You can distribute that file to your friend.
+## Windows:
 
-License:
-----------
+Run the setup.py file like this:
+
+```bash
+python setup.py pyinstaller
+```
+
+This will result in a **dist** and **build** directory. Simply go to the **dist** directory and you will get a ```youtube-dl-gui.exe``` file and some other files. That ```youtube-dl-gui.exe``` file is a standalone executable which can be run simply by double clicking it. You can distribute that file to your friend.
+
+## License:
+
 This project is released under the MIT license. See the included license file.
--- setup.py
+++ setup.py
@@ -1,31 +1,204 @@
-from distutils.core import setup
-import py2exe, sys, os
+# -*- coding: utf-8 -*-
 
-sys.argv.append('py2exe')
+"""Youtube-dl-gui setup file.
 
-from distutils.sysconfig import get_python_lib
-site_packages_path = get_python_lib()
-plugins_path = os.path.join(site_packages_path, 'PyQt4', 'plugins', 'imageformats')
-image_format_dlls = map(
-    lambda dll: os.path.join(plugins_path, dll),
-    ['qjpeg4.dll', 'qgif4.dll', 'qico4.dll', 'qmng4.dll', 'qsvg4.dll', 'qtiff4.dll']
-)
+Examples:
+    Windows/Linux::
 
-DATA=[('imageformats', image_format_dlls), ('', ['.\\ffmpeg.exe'])]
+        python setup.py pyinstaller
 
-for files in os.listdir(os.path.join(os.getcwd(),'UI')):
-    f1 = os.path.join(os.getcwd(),'UI', files)
-    if os.path.isfile(f1): # skip directories
-        f2 = 'UI', [f1]
-        DATA.append(f2)
+    Linux::
+
+        python setup.py install
+
+    Build source distribution::
+
+        python setup.py sdist
+
+    Build platform distribution::
+
+        python setup.py bdist
+
+"""
+
+import os
+import sys
+from setuptools import setup, Command
+from distutils.spawn import spawn
+import time
+
+
+__packagename__ = "youtube_dl_gui"
+
+PYINSTALLER = len(sys.argv) >= 2 and sys.argv[1] == "pyinstaller"
+
+try:
+    from PyInstaller import compat as pyi_compat
+
+    if pyi_compat.is_win:
+        # noinspection PyUnresolvedReferences
+        from PyInstaller.utils.win32.versioninfo import (
+            VarStruct, VarFileInfo, StringStruct, StringTable,
+            StringFileInfo, FixedFileInfo, VSVersionInfo, SetVersion,
+        )
+except ImportError:
+    pyi_compat = None
+    if PYINSTALLER:
+        print("Cannot import pyinstaller", file=sys.stderr)
+        exit(1)
+
+# Get the info from UI/__init__.py without importing the package
+exec(compile(open("UI/__init__.py").read(), "UI/__init__.py", "exec"))
+
+DESCRIPTION = __description__
+LONG_DESCRIPTION = __descriptionfull__
+
+
+def on_windows():
+    """Returns True if OS is Windows."""
+    return os.name == "nt"
+
+
+def version2tuple(commit=0):
+    version_list = str(__version__).split(".")
+    if len(version_list) > 3:
+        _commit = int(version_list[3])
+        del version_list[3]
+    else:
+        _commit = commit
+
+    _year, _month, _day = [int(value) for value in version_list]
+    return _year, _month, _day, _commit
+
+
+def version2str(commit=0):
+    version_tuple = version2tuple(commit)
+    return "%s.%s.%s.%s" % version_tuple
+
+
+class BuildPyinstallerBin(Command):
+    description = "Build the executable"
+    user_options = []
+    version_file = None
+    if pyi_compat and pyi_compat.is_win:
+        version_file = VSVersionInfo(
+            ffi=FixedFileInfo(
+                filevers=version2tuple(),
+                prodvers=version2tuple(),
+                mask=0x3F,
+                flags=0x0,
+                OS=0x4,
+                fileType=0x1,
+                subtype=0x0,
+                date=(0, 0),
+            ),
+            kids=[
+                VarFileInfo([VarStruct("Translation", [0, 1200])]),
+                StringFileInfo(
+                    [
+                        StringTable(
+                            "000004b0",
+                            [
+                                StringStruct("CompanyName", __maintainer_contact__),
+                                StringStruct("FileDescription", DESCRIPTION),
+                                StringStruct("FileVersion", version2str()),
+                                StringStruct("InternalName", "youtube-dl-gui.exe"),
+                                StringStruct(
+                                    "LegalCopyright",
+                                    __projecturl__ + "LICENSE",
+                                ),
+                                StringStruct("OriginalFilename", "youtube-dl-gui.exe"),
+                                StringStruct("ProductName", __appname__),
+                                StringStruct("ProductVersion", version2str()),
+                            ],
+                        )
+                    ]
+                ),
+            ],
+        )
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self, version=version_file):
+        """Run pyinstaller"""
+        if on_windows():
+            path_sep = ";"
+        else:
+            path_sep = ":"
+        
+        spawn(
+            [
+                "pyinstaller",
+                "-w",
+                "-F",
+                "--icon=UI/images/icon.ico",
+                "--add-data=UI/images"+path_sep+"UI/images",
+                "--name=youtube-dl-gui",
+                "main.py",
+            ],
+            dry_run=self.dry_run)
+
+        if version:
+            time.sleep(3)
+            SetVersion("./dist/youtube-dl-gui.exe", version)
+
+
+def setup_data():
+    """Setup params for Windows/Linux"""
+    # Add pixmaps icons (*.ico,*.png)
+    package_data = {"UI": [
+        "images/*.ico",
+        "images/*.png"
+    ]}
+    setup_params = {
+       "package_data": package_data,
+    }
+
+    return setup_params
+
+cmdclass = dict()
+params = dict()
+
+if PYINSTALLER:
+    cmdclass.update({"pyinstaller": BuildPyinstallerBin})
+else:
+    params = setup_data()
+    params["entry_points"] = {
+        "console_scripts": ["youtube-dl-gui = main:main"]
+    }
+
 
 setup(
-    options = {'py2exe': {'compressed': True,"includes":["sip"]}},
-    windows = [{
-                   'script': "main.py",
-                   "icon_resources": [(0, os.path.join(os.getcwd(),"resources","converted_icon.ico"))],
-                   "dest_base":"youtube-gl",
-               }],
-    zipfile = None,
-    data_files = DATA,
+    name=__packagename__,
+    version=__version__,
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    url=__projecturl__,
+    author=__author__,
+    author_email=__contact__,
+    maintainer=__maintainer__,
+    maintainer_email=__maintainer_contact__,
+    license=__license__,
+    py_modules=["main"],
+    packages=["GUI", "Threads", "UI", "youtube_dl", "youtube_dl.downloader", "youtube_dl.extractor", "youtube_dl.postprocessor"],
+    include_package_data=True,
+    classifiers=[
+        "Topic :: Multimedia :: Video :: User Interfaces",
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: MacOS X :: Cocoa",
+        "Environment :: Win32 (MS Windows)",
+        "Environment :: X11 Applications :: GTK",
+        "License :: Public Domain",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: Implementation",
+        "Programming Language :: Python :: Implementation :: CPython",
+    ],
+    cmdclass=cmdclass,
+    **params
 )
